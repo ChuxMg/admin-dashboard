e548a41ba348c0f421f15451c4fe2405e5e26bf4
ChuxMg <chuxmgbojikwe@gmail.com>
2024-08-16 17:52:47 +0700

    Implement Sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import { Button } from "@/components/ui/button";
+
 
 export default function Home() {
   return (
     <>
     <h1 className="text-2xl">Dashboard</h1>
-    <Button variant="destructive">Click Me!</Button>
     </>
   );
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import Link from "next/link";
+import logo from "../img/chux-profile-photo2.png";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+
+const Navbar = () => {
+  return (
+    <div className="bg-primary dark:bg-slate-700 text-white py-2 px-5 flex justify-between">
+      Navbar
+      <Link href="/">
+        <Image src={logo} alt="logo" width={40} />
+      </Link>
+      <DropdownMenu>
+        <DropdownMenuTrigger className="focus:outline-none">
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+            <AvatarFallback className="text-black">CM</AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <Link href="/">Profile</Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Link href="/auth">Logout</Link>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
+
+export default Navbar;
